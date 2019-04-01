ae701484399a62fe997eface7ac276b752c5275d
j-Riv <jriv@suavecito.com>
2019-04-01 15:16:55 -0700

    Updated Model

--- models/book.js
+++ models/book.js
@@ -2,11 +2,18 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const bookSchema = new Schema({
-    title: { type: String, required: true },
+    title: { 
+        type: String, 
+        unique: true, 
+        required: true 
+    },
     authors: [{ type: String}],
     description: String,
     image: String,
-    link: { type: String, required: true }
+    link: { 
+        type: String, 
+        required: true 
+    }
 });
 
 const Book = mongoose.model('Book', bookSchema);
